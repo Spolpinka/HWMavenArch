fd316255f363640b506d4e38e5b40f1c2b2ff73d
Spolpinka <a421243266@gmail.com>
2023-06-14 07:31:09 +0300

    полный CRUD по employee

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -10,28 +10,28 @@ import lombok.*;
 @EqualsAndHashCode(of = "id")
 public class Employee {
     private int id;
-    private String first_name;
-    private String last_name;
+    private String firstName;
+    private String lastName;
     private String gender;
     private int age;
-    private int city_id;
+    private int cityId;
 
-    public Employee(String first_name, String last_name, String gender, int age, int city_id) {
-        this.first_name = first_name;
-        this.last_name = last_name;
+    public Employee(String firstName, String lastName, String gender, int age, int city_id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.gender = gender;
         this.age = age;
-        this.city_id = city_id;
+        this.cityId = city_id;
     }
 
     @Override
     public String toString() {
         return "Сотрудник: " +
                 " id= " + id +
-                ", first_name= " + first_name +
-                ", last_name= " + last_name +
+                ", first_name= " + firstName +
+                ", last_name= " + lastName +
                 ", gender= " + gender +
                 ", age= " + age +
-                ", city_id=" + city_id;
+                ", city_id=" + cityId;
     }
 }
